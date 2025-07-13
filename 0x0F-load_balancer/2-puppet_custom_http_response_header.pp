70ce79a21762f218892833a2f9dc6b101f756808
Kirubel Kinfe <kirubelk@act.com.et>
2025-07-13 14:06:21 +0300

    added https ssl

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,50 +1,30 @@
-# Installs and configures Nginx with a custom HTTP header X-Served-By containing the hostname
+# Setup New Ubuntu server with nginx
+# and add a custom HTTP header
 
-# Ensure the Nginx package is installed
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
 package { 'nginx':
-  ensure => installed,
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-# Create the index.html file with "Hello World!"
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => "Hello World!\n",
-  require => Package['nginx'],
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-# Get the hostname dynamically
-$hostname = $facts['networking']['hostname']
-
-# Configure Nginx to listen on port 80 and add the X-Served-By header
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => @("EOF")
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html;
-
-    server_name _;
-
-    add_header X-Served-By ${hostname};
-
-    location / {
-        try_files \$uri \$uri/ /index.html;
-    }
-}
-EOF
-  ,
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  require    => [Package['nginx'], File['/etc/nginx/sites-available/default']],
-  hasrestart => true,
-  restart    => '/usr/sbin/service nginx reload',
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
